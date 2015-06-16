429f67b4a7950686c669cb4336f5b71e330db9d2
Jesse <jman765@gmail.com>
2015-06-15 20:49:34 -0400

    fixed gulpfile not running properly

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,31 @@
+'use strict';
+
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var jshint = require('jshint-stylish');
 var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
 
 gulp.task('sass', function() {
-  return gulp.src(['src/sass/main.scss'])
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
-    .pipe(gulp.dest('../public/css/'))
-    .pipe(livereload());
+    gulp.src(['src/sass/main.scss'])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./public/css'))
+        .pipe(livereload());
 });
 
 gulp.task('js', function() {
-  return gulp.src('src/js/*.js')
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(gulp.dest('../public/js/'));
+    gulp.src('src/js/*.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(gulp.dest('./public/js'));
 });
 
 gulp.task('watch', function() {
     livereload.listen();
     gulp.watch('src/js/*.js', ['js']);
-    gulp.watch('src/sass/main.scss', ['sass']);
+    gulp.watch('src/sass/**/*.scss', ['sass']);
 });
