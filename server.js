2e7a70d19a378c76c0c0095d127061677045d0fd
Jesse Breneman <jmwashere@gmail.com>
2015-08-14 22:16:46 -0400

    removed redundant http require

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
 
 app.use('/', express.static('public'));
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/public/index.html');
+	res.sendFile('./public/index.html');
 });
 
-http.listen(3000, function() {
+app.listen(3000, function() {
 	console.log('Server started on port 3000')
 });
